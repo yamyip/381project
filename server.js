8f5765cb0cd54deb216f662446a8fd630f37afe1
yamyip <121743478+yamyip@users.noreply.github.com>
2023-11-22 17:51:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,13 +20,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
     secret: 'your_secret_key',
     resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({ 
-        url: 'mongodb+srv://yamyip:Nn785412@cluster0.srzd1zr.mongodb.net/?retryWrites=true&w=majority', // Update with your MongoDB connection string
-        ttl: 60 * 60, // Session TTL (in seconds), optional
-    }),
+    saveUninitialized: false
 }));
-
 app.get('/', function(req, res){
 	if(!req.session.authenticated){
 		console.log("Not authenticated; directing to login");
